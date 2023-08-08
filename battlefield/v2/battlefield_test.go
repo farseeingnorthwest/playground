dd9705fbd957bf4bf1d300b97e187fc84b815662
tsai phan <phanalpha@hotmail.com>
2023-08-08 17:19:34 +0800

    multi-step critical.

--- battlefield/v2/battlefield_test.go
+++ battlefield/v2/battlefield_test.go
@@ -6,8 +6,8 @@ import (
 	"golang.org/x/exp/slog"
 )
 
-func ExampleBattleField_Run() {
-	h := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+func ExampleBattleField_Run_special_0() {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
 			if len(groups) == 0 {
@@ -19,8 +19,8 @@ func ExampleBattleField_Run() {
 
 			return a
 		},
-	})
-	slog.SetDefault(slog.New(h))
+	})))
+
 	rng.SetRng(NewSequence(0.1, 0.5))
 	f := NewBattleField(
 		[]Warrior{
@@ -34,12 +34,13 @@ func ExampleBattleField_Run() {
 				},
 				Left,
 				0,
-				T0[0],
-				T0[1],
-				T0[2],
-				T0[3],
-				T0[4],
-				T0[5],
+				Regular[0],
+				Regular[1],
+				Regular[2],
+				Special[0][0],
+				Special[0][1],
+				Special[0][2],
+				Special[0][3],
 			),
 			NewMyWarrior(
 				MyBaseline{
@@ -49,7 +50,7 @@ func ExampleBattleField_Run() {
 				},
 				Right,
 				0,
-				T0[0],
+				Regular[0],
 			),
 		},
 	)
@@ -57,11 +58,125 @@ func ExampleBattleField_Run() {
 	// Output:
 	// verb=buff reactor="+20% Damage" target.side=Left target.position=0 source.reactor="@BattleStart({$} +20% Damage)"
 	// verb=buff reactor="[15] +2% CriticalOdds" target.side=Left target.position=0 source.reactor="@Launch([15] +2% CriticalOdds)"
-	// verb=attack critical=true loss=98 overflow=0 source.side=Left source.position=0 source.reactor="@Launch({*} 480% Damage, {1} 520% Damage)" source.damage=57 target.side=Right target.position=0 target.defense=8 target.health.current=102 target.health.maximum=200
-	// verb=attack critical=false loss=54 overflow=0 source.side=Left source.position=0 source.reactor="@Launch({*} 480% Damage, {1} 520% Damage)" source.damage=62 target.side=Right target.position=0 target.defense=8 target.health.current=48 target.health.maximum=200
+	// verb=attack critical=true loss=98 overflow=0 source.side=Left source.position=0 source.reactor="@Launch({*} 480% Damage; {1} 520% Damage)" source.damage=57 target.side=Right target.position=0 target.defense=8 target.health.current=102 target.health.maximum=200
+	// verb=attack critical=false loss=54 overflow=0 source.side=Left source.position=0 source.reactor="@Launch({*} 480% Damage; {1} 520% Damage)" source.damage=62 target.side=Right target.position=0 target.defense=8 target.health.current=48 target.health.maximum=200
 	// verb=attack critical=false loss=7 overflow=0 source.side=Right source.position=0 source.reactor=NormalAttack source.damage=12 target.side=Left target.position=0 target.defense=5 target.health.current=93 target.health.maximum=100
 	// verb=buff reactor="[15] +2% CriticalOdds" target.side=Left target.position=0 source.reactor="@Launch([15] +2% CriticalOdds)"
 	// verb=attack critical=false loss=47 overflow=0 source.side=Left source.position=0 source.reactor="@Launch({1} 3 * 460% Damage)" source.damage=55 target.side=Right target.position=0 target.defense=8 target.health.current=1 target.health.maximum=200
 	// verb=attack critical=false loss=47 overflow=46 source.side=Left source.position=0 source.reactor="@Launch({1} 3 * 460% Damage)" source.damage=55 target.side=Right target.position=0 target.defense=8 target.health.current=0 target.health.maximum=200
 	// verb=attack critical=false loss=47 overflow=47 source.side=Left source.position=0 source.reactor="@Launch({1} 3 * 460% Damage)" source.damage=55 target.side=Right target.position=0 target.defense=8 target.health.current=0 target.health.maximum=200
+}
+
+func ExampleBattleField_Run_special_1() {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if len(groups) == 0 {
+				switch a.Key {
+				case slog.TimeKey, slog.LevelKey, slog.MessageKey:
+					return slog.Attr{}
+				}
+			}
+
+			return a
+		},
+	})))
+
+	rng.SetRng(NewSequence(0.1, 0.9))
+	f := NewBattleField(
+		[]Warrior{
+			NewMyWarrior(
+				MyBaseline{
+					Damage:  10,
+					Defense: 5,
+					Health:  100,
+				},
+				Left,
+				0,
+				Regular[0],
+				Special[1][0],
+				Special[1][1],
+			),
+			NewMyWarrior(
+				MyBaseline{
+					Damage:  12,
+					Defense: 8,
+					Health:  84,
+				},
+				Right,
+				0,
+				Regular[0],
+			),
+		},
+	)
+	f.Run()
+	// Output:
+	// verb=attack critical=false loss=44 overflow=0 source.side=Left source.position=0 source.reactor="@Launch({1} 520% Damage, P(70%, Dizzy))" source.damage=52 target.side=Right target.position=0 target.defense=8 target.health.current=40 target.health.maximum=84
+	// verb=buff reactor=Dizzy target.side=Right target.position=0 source.reactor="@Launch({1} 520% Damage, P(70%, Dizzy))"
+	// verb=attack critical=false loss=34 overflow=0 source.side=Left source.position=0 source.reactor="@Launch({2} 420% Damage, +30% Loss, Sleeping)" source.damage=42 target.side=Right target.position=0 target.defense=8 target.health.current=6 target.health.maximum=84
+	// verb=buff reactor="+30% Loss" target.side=Right target.position=0 source.reactor="@Launch({2} 420% Damage, +30% Loss, Sleeping)"
+	// verb=buff reactor=Sleeping target.side=Right target.position=0 source.reactor="@Launch({2} 420% Damage, +30% Loss, Sleeping)"
+	// verb=attack critical=false loss=2 overflow=0 source.side=Left source.position=0 source.reactor=NormalAttack source.damage=10 target.side=Right target.position=0 target.defense=8 target.health.current=4 target.health.maximum=84
+	// verb=attack critical=false loss=7 overflow=0 source.side=Right source.position=0 source.reactor=NormalAttack source.damage=12 target.side=Left target.position=0 target.defense=5 target.health.current=93 target.health.maximum=100
+	// verb=attack critical=false loss=2 overflow=0 source.side=Left source.position=0 source.reactor=NormalAttack source.damage=10 target.side=Right target.position=0 target.defense=8 target.health.current=2 target.health.maximum=84
+	// verb=attack critical=false loss=7 overflow=0 source.side=Right source.position=0 source.reactor=NormalAttack source.damage=12 target.side=Left target.position=0 target.defense=5 target.health.current=86 target.health.maximum=100
+	// verb=attack critical=false loss=44 overflow=42 source.side=Left source.position=0 source.reactor="@Launch({1} 520% Damage, P(70%, Dizzy))" source.damage=52 target.side=Right target.position=0 target.defense=8 target.health.current=0 target.health.maximum=84
+}
+
+func ExampleBattleField_Run_special_2() {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if len(groups) == 0 {
+				switch a.Key {
+				case slog.TimeKey, slog.LevelKey, slog.MessageKey:
+					return slog.Attr{}
+				}
+			}
+
+			return a
+		},
+	})))
+
+	rng.SetRng(NewSequence(0.1, 0.9))
+	f := NewBattleField(
+		[]Warrior{
+			NewMyWarrior(
+				MyBaseline{
+					Damage:       10,
+					Defense:      5,
+					CriticalLoss: 200,
+					Health:       100,
+				},
+				Left,
+				0,
+				Regular[0],
+				Regular[1],
+				Regular[2],
+				Special[2][0],
+				Special[2][1],
+				Special[2][2],
+				Special[2][3],
+			),
+			NewMyWarrior(
+				MyBaseline{
+					Damage:  12,
+					Defense: 8,
+					Health:  130,
+				},
+				Right,
+				0,
+				Regular[0],
+			),
+		},
+	)
+	f.Run()
+	// Output:
+	// verb=buff reactor="+2% Attack*" target.side=Left target.position=0 source.reactor="@BattleStart({~} +2% Attack*)"
+	// verb=buff reactor="+25% HealthMaximum" target.side=Left target.position=0 source.reactor="@BattleStart({$} +25% HealthMaximum)"
+	// verb=attack critical=true loss=64 overflow=0 source.side=Left source.position=0 source.reactor="@Launch({1!/%} 400% Critical Damage)" source.damage=40 target.side=Right target.position=0 target.defense=8 target.health.current=66 target.health.maximum=130
+	// verb=attack critical=false loss=7 overflow=0 source.side=Right source.position=0 source.reactor=NormalAttack source.damage=12 target.side=Left target.position=0 target.defense=5 target.health.current=118 target.health.maximum=125
+	// verb=attack critical=true loss=64 overflow=0 source.side=Left source.position=0 source.reactor="@Launch({1^/%} 400% Critical Damage)" source.damage=40 target.side=Right target.position=0 target.defense=8 target.health.current=2 target.health.maximum=130
+	// verb=attack critical=false loss=7 overflow=0 source.side=Right source.position=0 source.reactor=NormalAttack source.damage=12 target.side=Left target.position=0 target.defense=5 target.health.current=111 target.health.maximum=125
+	// verb=attack critical=false loss=2 overflow=0 source.side=Left source.position=0 source.reactor=NormalAttack source.damage=10 target.side=Right target.position=0 target.defense=8 target.health.current=0 target.health.maximum=130
 }
--- battlefield/v2/skill.go
+++ battlefield/v2/skill.go
@@ -1,26 +1,24 @@
 package battlefield
 
 var (
-	rng   = &RngProxy{}
-	Skill = ExclusionGroup(0)
-	T0    = []Reactor{
-		// 0
+	rng        = &RngProxy{}
+	SkillGroup = ExclusionGroup(0)
+	Regular    = []Reactor{
 		NewFatReactor(
-			FatTags(Skill, Label("NormalAttack")),
+			FatTags(SkillGroup, Label("NormalAttack")),
 			FatRespond(
 				NewSignalTrigger(&LaunchSignal{}),
 				NewSelectActor(
 					NewVerbActor(&Attack{}, AxisEvaluator(Damage)),
-					Healthy,
 					SideSelector(false),
+					Healthy,
 					NewShuffleSelector(rng, nil),
 					FrontSelector(1),
 				),
 			),
 		),
-
-		// 1
 		NewFatReactor(
+			FatTags(Priority(1000)),
 			FatRespond(
 				NewFatTrigger(
 					&PreActionSignal{},
@@ -30,101 +28,273 @@ var (
 				NewProbabilityActor(
 					rng,
 					AxisEvaluator(CriticalOdds),
-					NewSequenceActor(
-						CriticalActor{},
-						NewActionBuffer(
-							AxisEvaluator(CriticalLoss),
-							NewBuffer(Loss, false, nil),
-						),
-					),
+					CriticalActor{},
 				),
 			),
 		),
+		NewFatReactor(
+			FatTags(Priority(999)),
+			FatRespond(
+				NewFatTrigger(
+					&PreActionSignal{},
+					CurrentIsSourceTrigger{},
+					NewVerbTrigger[*Attack](),
+					CriticalStrikeTrigger{},
+				),
+				NewActionBuffer(
+					AxisEvaluator(CriticalLoss),
+					NewBuffer(Loss, false, nil),
+				),
+			),
+		),
+	}
+
+	Special = [][]Reactor{
+		// ////////////////////////////////////////////////////////////
+		// [0] 織田
+		{
+			// 對隨機1名敵人進行3次攻擊，每次造成攻擊力460%的傷害。
+			NewFatReactor(
+				FatTags(SkillGroup, Priority(1), Label("@Launch({1} 3 * 460% Damage)")),
+				FatRespond(
+					NewSignalTrigger(&LaunchSignal{}),
+					NewSelectActor(
+						NewSequenceActor(
+							NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 460)),
+							NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 460)),
+							NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 460)),
+						),
+						SideSelector(false),
+						Healthy,
+						NewShuffleSelector(rng, nil),
+						FrontSelector(1),
+					),
+				),
+				FatCooling(NewSignalTrigger(&RoundEndSignal{}), 5),
+			),
+
+			// 對全體敵人造成攻擊力480%的傷害。再對當前生命值最低的1名敵人造成攻擊力520%的傷害。
+			NewFatReactor(
+				FatTags(SkillGroup, Priority(2), Label("@Launch({*} 480% Damage; {1} 520% Damage)")),
+				FatRespond(
+					NewSignalTrigger(&LaunchSignal{}),
+					NewSelectActor(
+						NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 480)),
+						SideSelector(false),
+						Healthy,
+					),
+					NewSelectActor(
+						NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 520)),
+						SideSelector(false),
+						Healthy,
+						NewSortSelector(HealthPercent, true),
+						FrontSelector(1),
+					),
+				),
+				FatCooling(NewSignalTrigger(&RoundEndSignal{}), 4),
+			),
+
+			// 每次行動開始時，提升2%爆擊率(最高15層，無法被解除)。
+			NewFatReactor(
+				FatTags(Priority(3), Label("@Launch([15] +2% CriticalOdds)")),
+				FatRespond(
+					NewSignalTrigger(&LaunchSignal{}),
+					NewSelectActor(
+						NewVerbActor(
+							NewBuff(
+								nil,
+								NewBuffReactor(CriticalOdds, true, ConstEvaluator(2), FatTags(
+									Label("[15] +2% CriticalOdds"))),
+							),
+							nil,
+						),
+						CurrentSelector{},
+					),
+				),
+			),
+
+			// 提升20%攻擊力。(無法被解除)
+			NewFatReactor(
+				FatTags(Priority(4), Label("@BattleStart({$} +20% Damage)")),
+				FatRespond(
+					NewSignalTrigger(&BattleStartSignal{}),
+					NewSelectActor(
+						NewVerbActor(
+							NewBuff(nil, NewBuffReactor(
+								Damage,
+								false,
+								ConstEvaluator(120),
+								FatTags(Label("+20% Damage")))),
+							nil,
+						),
+						CurrentSelector{},
+					),
+				),
+			),
+		},
 
 		// ////////////////////////////////////////////////////////////
-		// 織田
-
-		// 對隨機1名敵人進行3次攻擊，每次造成攻擊力460%的傷害。
-		// 2
-		NewFatReactor(
-			FatTags(Skill, Priority(1), Label("@Launch({1} 3 * 460% Damage)")),
-			FatRespond(
-				NewSignalTrigger(&LaunchSignal{}),
-				NewSelectActor(
-					NewSequenceActor(
-						NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 460)),
-						NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 460)),
-						NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 460)),
-					),
-					Healthy,
-					SideSelector(false),
-					NewShuffleSelector(rng, nil),
-					FrontSelector(1),
-				),
-			),
-			FatCooling(NewSignalTrigger(&RoundEndSignal{}), 5),
-		),
-
-		// 對全體敵人造成攻擊力480%的傷害。再對當前生命值最低的1名敵人造成攻擊力520%的傷害。
-		// 3
-		NewFatReactor(
-			FatTags(Skill, Priority(2), Label("@Launch({*} 480% Damage, {1} 520% Damage)")),
-			FatRespond(
-				NewSignalTrigger(&LaunchSignal{}),
-				NewSelectActor(
-					NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 480)),
-					Healthy,
-					SideSelector(false),
-				),
-				NewSelectActor(
-					NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 520)),
-					Healthy,
-					SideSelector(false),
-					NewSortSelector(HealthPercent, true),
-					FrontSelector(1),
-				),
-			),
-			FatCooling(NewSignalTrigger(&RoundEndSignal{}), 4),
-		),
-
-		// 每次行動開始時，提升2%爆擊率(最高15層，無法被解除)。
-		// 4
-		NewFatReactor(
-			FatTags(Priority(3), Label("@Launch([15] +2% CriticalOdds)")),
-			FatRespond(
-				NewSignalTrigger(&LaunchSignal{}),
-				NewSelectActor(
-					NewVerbActor(
-						NewBuff(
-							nil,
-							NewBuffReactor(CriticalOdds, true, ConstEvaluator(2), FatTags(
-								Label("[15] +2% CriticalOdds"))),
+		// [1] 豐臣
+		{
+			// 對隨機2名敵人造成攻擊力420%的傷害。並對目標附加30%被擊增傷(1回合)&「沉睡」(1回合)。
+			NewFatReactor(
+				FatTags(SkillGroup, Priority(1), Label("@Launch({2} 420% Damage, +30% Loss, Sleeping)")),
+				FatRespond(
+					NewSignalTrigger(&LaunchSignal{}),
+					NewSelectActor(
+						NewSequenceActor(
+							NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 420)),
+							NewVerbActor(
+								// 30% 被擊增傷 (1回合)
+								NewBuff(nil, NewBuffReactor(
+									Loss,
+									false,
+									ConstEvaluator(130),
+									FatTags(Label("+30% Loss")),
+									FatCapacity(NewSignalTrigger(&RoundEndSignal{}), 1))),
+								nil,
+							),
+							// 「沉睡」 (1回合)
+							NewVerbActor(
+								NewBuff(nil, NewFatReactor(
+									FatTags(SkillGroup, Priority(10), Label("Sleeping")),
+									FatRespond(
+										NewSignalTrigger(&LaunchSignal{}),
+										NewSequenceActor(),
+									),
+									FatCapacity(
+										NewOrTrigger(
+											NewSignalTrigger(&RoundEndSignal{}),
+											NewFatTrigger(
+												&PostActionSignal{},
+												VerbTrigger[*Attack]{},
+												CurrentIsTargetTrigger{}),
+										),
+										1,
+									),
+								)),
+								nil,
+							),
 						),
-						nil,
+						SideSelector(false),
+						Healthy,
+						NewShuffleSelector(rng, nil),
+						FrontSelector(2),
 					),
-					CurrentSelector{},
 				),
+				FatCooling(NewSignalTrigger(&RoundEndSignal{}), 5),
 			),
-		),
 
-		// 提升20%攻擊力。(無法被解除)
-		// 5
-		NewFatReactor(
-			FatTags(Priority(4), Label("@BattleStart({$} +20% Damage)")),
-			FatRespond(
-				NewSignalTrigger(&BattleStartSignal{}),
-				NewSelectActor(
-					NewVerbActor(
-						NewBuff(
-							nil,
-							NewBuffReactor(Damage, false, ConstEvaluator(120), FatTags(
-								Label("+20% Damage"))),
+			// 對攻擊力最高的1名敵人造成攻擊力520%的傷害。並有70%機率對目標附加「暈眩」(1回合)。
+			NewFatReactor(
+				FatTags(SkillGroup, Priority(2), Label("@Launch({1} 520% Damage, P(70%, Dizzy))")),
+				FatRespond(
+					NewSignalTrigger(&LaunchSignal{}),
+					NewSelectActor(
+						NewSequenceActor(
+							NewVerbActor(&Attack{}, NewMultiplier(AxisEvaluator(Damage), 520)),
+							// 「暈眩」 (1回合)
+							NewProbabilityActor(rng, ConstEvaluator(70), NewVerbActor(
+								NewBuff(nil, NewFatReactor(
+									FatTags(SkillGroup, Priority(10), Label("Dizzy")),
+									FatRespond(
+										NewSignalTrigger(&LaunchSignal{}),
+										NewSequenceActor(),
+									),
+									FatCapacity(
+										NewSignalTrigger(&RoundEndSignal{}),
+										1,
+									),
+								)),
+								nil,
+							)),
 						),
-						nil,
+						SideSelector(false),
+						Healthy,
+						NewSortSelector(Damage, false),
+						FrontSelector(1),
 					),
-					CurrentSelector{},
+				),
+				FatCooling(NewSignalTrigger(&RoundEndSignal{}), 4),
+			),
+
+			// 自身附帶2種以上減益效果時觸發，解除所有減益效果。
+
+			// 行動開始時獲得「多重屏障」(2層，無法被解除)。
+		},
+
+		// ////////////////////////////////////////////////////////////
+		// [2] 上杉
+		{
+			// 對當前生命值百分比最高的 1 名敵人造成攻擊力 400% 的傷害；此攻擊必定爆擊。
+			NewFatReactor(
+				FatTags(SkillGroup, Priority(1), Label("@Launch({1^/%} 400% Critical Damage)")),
+				FatRespond(
+					NewSignalTrigger(&LaunchSignal{}),
+					NewSelectActor(
+						NewVerbActor(NewAttack(nil, true), NewMultiplier(AxisEvaluator(Damage), 400)),
+						SideSelector(false),
+						Healthy,
+						NewSortSelector(HealthPercent, false),
+						FrontSelector(1),
+					),
+				),
+				FatCooling(NewSignalTrigger(&RoundEndSignal{}), 3),
+			),
+
+			// 對當前生命值百分比最低的 1 名敵人造成攻擊力400%的傷害；此攻擊必定爆擊。
+			NewFatReactor(
+				FatTags(SkillGroup, Priority(2), Label("@Launch({1!/%} 400% Critical Damage)")),
+				FatRespond(
+					NewSignalTrigger(&LaunchSignal{}),
+					NewSelectActor(
+						NewVerbActor(NewAttack(nil, true), NewMultiplier(AxisEvaluator(Damage), 400)),
+						SideSelector(false),
+						Healthy,
+						NewSortSelector(HealthPercent, true),
+						FrontSelector(1),
+					),
+				),
+				FatCooling(NewSignalTrigger(&RoundEndSignal{}), 3),
+			),
+
+			// 提升25%最大生命值(無法被解除)。
+			NewFatReactor(
+				FatTags(SkillGroup, Priority(3), Label("@BattleStart({$} +25% HealthMaximum)")),
+				FatRespond(
+					NewSignalTrigger(&BattleStartSignal{}),
+					NewSelectActor(
+						NewVerbActor(
+							NewBuff(nil, NewBuffReactor(
+								HealthMaximum,
+								false,
+								ConstEvaluator(125),
+								FatTags(Label("+25% HealthMaximum")))),
+							nil,
+						),
+						CurrentSelector{},
+					),
 				),
 			),
-		),
+
+			// 戰鬥開始時，每有一名友軍，全體友軍提升2%攻擊力。(無法被解除)
+			NewFatReactor(
+				FatTags(SkillGroup, Priority(4), Label("@BattleStart({~} +2% Attack*)")),
+				FatRespond(
+					NewSignalTrigger(&BattleStartSignal{}),
+					NewSelectActor(
+						NewVerbActor(
+							NewBuff(nil, NewBuffReactor(
+								Damage,
+								false,
+								ConstEvaluator(102), // TODO:
+								FatTags(Label("+2% Attack*")))),
+							nil,
+						),
+						SideSelector(true),
+					),
+				),
+			),
+		},
 	}
 )
